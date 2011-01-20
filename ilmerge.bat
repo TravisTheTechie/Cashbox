e62805630e068a316c62c2548f38bbf1f0fb8735
Travis Smith <travis@legomaster.net>
2011-01-20 17:28:07 -0500

    Adding a step to copy to an output dir so we can pack it for nu.

--- ilmerge.bat
+++ ilmerge.bat
@@ -1,2 +1,5 @@
+mkdir output\lib
 cd src\Cashbox\bin\Release
-..\..\..\..\lib\ILMerge\ILMerge.exe/internalize /target:dll /out:..\Cashbox.dll /ndebug /allowDup Cashbox.dll Community.CsharpSqlite.dll Community.CsharpSqlite.SQLiteClient.dll Magnum.dll Stact.dll System.Threading.dll
+..\..\..\..\lib\ILMerge\ILMerge.exe/internalize /target:dll /out:..\Cashbox.dll /ndebug /allowDup Cashbox.dll Magnum.dll Stact.dll System.Threading.dll
+move /y ..\Cashbox.dll ..\..\..\..\output\lib
+cd ..\..\..\..
